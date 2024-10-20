2d635bc9acba18cb9d912e3beb3c8a7dbb786608
Sayandip Jana <jsupriti3434@gmail.com>
2024-10-20 17:04:46 +0530

    Final Commit

---
+++ components/navbar.tsx
@@ -0,0 +1,179 @@
+import Logo from './logo'
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+import {
+  LinkedInLogoIcon,
+  GitHubLogoIcon,
+  TwitterLogoIcon,
+} from '@radix-ui/react-icons'
+import { Session } from '@supabase/supabase-js'
+import {
+  ArrowRight,
+  LogOut,
+  MoonIcon,
+  SunIcon,
+  Trash,
+  Undo,
+} from 'lucide-react'
+import { useTheme } from 'next-themes'
+import Link from 'next/link'
+
+export function NavBar({
+  session,
+  showLogin,
+  signOut,
+  onClear,
+  canClear,
+  onSocialClick,
+  onUndo,
+  canUndo,
+}: {
+  session: Session | null
+  showLogin: () => void
+  signOut: () => void
+  onClear: () => void
+  canClear: boolean
+  onSocialClick: (target: 'github' | 'x' | 'linkedin') => void
+  onUndo: () => void
+  canUndo: boolean
+}) {
+  const { setTheme, theme } = useTheme()
+  return (
+    <nav className="w-full flex py-4 pr-4 pl-4 mt-2 bg-gradient-to-br from-black/60 via-red-800/80 to-black/60 backdrop-blur-md shadow-lg rounded-lg transition-transform hover:scale-[1.02]">
+      <div className="flex flex-1 items-center">
+        <Link href="/" className="flex items-center gap-2" target="_blank">
+          <Logo width={24} height={24} />
+          <h1 className="whitespace-pre text-white">CodeGen - CodeCatalysts</h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-1 md:gap-4">
+        <TooltipProvider>
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={onUndo}
+                disabled={!canUndo}
+                className="text-white hover:bg-white/10"
+              >
+                <Undo className="h-4 w-4 md:h-5 md:w-5" />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Undo</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={onClear}
+                disabled={!canClear}
+                className="text-white hover:bg-white/10"
+              >
+                <Trash className="h-4 w-4 md:h-5 md:w-5" />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Clear chat</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                className="text-white hover:bg-white/10"
+              >
+                {theme === 'light' ? (
+                  <SunIcon className="h-4 w-4 md:h-5 md:w-5" />
+                ) : (
+                  <MoonIcon className="h-4 w-4 md:h-5 md:w-5" />
+                )}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Toggle theme</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        {session ? (
+          <DropdownMenu>
+            <TooltipProvider>
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger asChild>
+                  <DropdownMenuTrigger asChild>
+                    <Avatar className="w-8 h-8">
+                      <AvatarImage
+                        src={
+                          session.user.user_metadata?.avatar_url ||
+                          'https://avatar.vercel.sh/' + session.user.email
+                        }
+                        alt={session.user.email}
+                      />
+                    </Avatar>
+                  </DropdownMenuTrigger>
+                </TooltipTrigger>
+                <TooltipContent>My Account</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <DropdownMenuContent className="w-56" align="end">
+              <DropdownMenuLabel className="flex flex-col">
+                <span className="text-sm">My Account</span>
+                <span className="text-xs text-muted-foreground">
+                  {session.user.email}
+                </span>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={() => {
+                  window.open('https://e2b.dev', '_blank')
+                }}
+              >
+                <Logo className="mr-2 h-4 w-4 text-muted-foreground" />
+                About CodeCatalysts
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => onSocialClick('github')}>
+                <GitHubLogoIcon className="mr-2 h-4 w-4 text-muted-foreground" />
+                Star us on GitHub
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => onSocialClick('linkedin')}>
+                <LinkedInLogoIcon className="mr-2 h-4 w-4 text-muted-foreground" />
+                Join us on LinkedIn
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => onSocialClick('x')}>
+                <TwitterLogoIcon className="mr-2 h-4 w-4 text-muted-foreground" />
+                Follow us on X
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={signOut}>
+                <LogOut className="mr-2 h-4 w-4 text-muted-foreground" />
+                Sign out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <Button variant="default" onClick={showLogin} className="text-white">
+            Sign in
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </Button>
+        )}
+      </div>
+    </nav>
+  )
+}
